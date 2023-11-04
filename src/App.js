58c554146b27471b344fdd5e945370d7fa76204b
Kimc <32549359+Lilpha@users.noreply.github.com>
2023-11-04 13:23:07 +0000

    image selection

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 // useEffect 는 2개의 argu를 가진다. 우선 render상황에서도 한번만 실행하게 도와주는 함수
 //만약 App 함수안에 api호출이 있는데, 안에 state의 변경으로 인해 App을 재호출하게 되면 자연스레 느려짐.
 //방지하기 위해 useEffect 사용하는것임
@@ -6,16 +7,7 @@ import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/Movie/:id">
-          <Detail />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <img  src= {require("./images/A.jpg")}/>
   );
 }
 
