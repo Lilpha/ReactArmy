b82e2343590a8d74ffb03bdc4c03a71dca2e6c6f
Kimc <32549359+Lilpha@users.noreply.github.com>
2023-11-17 14:08:32 +0000

    home

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./style.css"
-// useEffect 는 2개의 argu를 가진다. 우선 render상황에서도 한번만 실행하게 도와주는 함수
-//만약 App 함수안에 api호출이 있는데, 안에 state의 변경으로 인해 App을 재호출하게 되면 자연스레 느려짐.
-//방지하기 위해 useEffect 사용하는것임
-import Home from "./routes/Home";
-import Detail from "./routes/Detail";
-function App() {
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import './style.css';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
+export default function App() {
   return (
-    <div className="Main">
-      <div className="slider">
-        <div className="LeftImage slide">
-          <img src={require("./images/A.jpg")}/>
-        </div>
-        <div className="CenterImage slide">
-        <img src={require("./images/B.jpg")}/>
-        </div>
-        <div className="RightImage slide">
-        <img src={require("./images/C.jpg")}/>
-        </div>
-      </div>
-    </div>
+    <>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src={require("./images/A.jpg")} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={require("./images/B.jpg")} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={require("./images/C.jpg")} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={require("./images/D.jpg")} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={require("./images/E.jpg")} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={require("./images/F.jpg")} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={require("./images/G.jpg")} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={require("./images/H.jpg")} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={require("./images/I.jpg")} />
+        </SwiperSlide>
+      </Swiper>
+    </>
   );
 }
-
-export default App;
---
+++ src/ttemp.js
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./style.css"
+// useEffect 는 2개의 argu를 가진다. 우선 render상황에서도 한번만 실행하게 도와주는 함수
+//만약 App 함수안에 api호출이 있는데, 안에 state의 변경으로 인해 App을 재호출하게 되면 자연스레 느려짐.
+//방지하기 위해 useEffect 사용하는것임
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+function App() {
+  return (
+    <div className="Main">
+      <div className="slider">
+        <div className="LeftImage slide">
+          <img src={require("./images/A.jpg")}/>
+        </div>
+        <div className="CenterImage slide">
+        <img src={require("./images/B.jpg")}/>
+        </div>
+        <div className="RightImage slide">
+        <img src={require("./images/C.jpg")}/>
+        </div>
+      </div>
+    </div>
+  );
+}
